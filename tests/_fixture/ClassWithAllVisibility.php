bbde13d8116b512cdb8e8e0a5fb33eb30f948c62
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2016-07-07 17:38:36 -0600

    Obtain protected methods when fetching the list of methods through the generator
    This resolves #314 where setMethodsExcept does not understand protected methods to be configurable

---
+++ tests/_fixture/ClassWithAllVisibility.php
@@ -0,0 +1,28 @@
+<?php
+
+class ClassWithAllVisibility
+{
+    public static function fooPublicStatic()
+    {
+    }
+
+    protected static function fooProtectedStatic()
+    {
+    }
+
+    private static function fooPrivateStatic()
+    {
+    }
+
+    public function fooPublic()
+    {
+    }
+
+    protected function fooProtected()
+    {
+    }
+
+    private function fooPrivate()
+    {
+    }
+}
