a3690fc3876a0d57b5d5494fcc53c77daa2401a7
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-12-12 14:05:42 -0500

    Add test for withConsecutiveArray

--- tests/MockObject/Matcher/ConsecutiveParametersTest.php
+++ tests/MockObject/Matcher/ConsecutiveParametersTest.php
@@ -18,6 +18,23 @@ class Framework_MockObject_Matcher_ConsecutiveParametersTest extends PHPUnit_Fra
         $this->assertNull($mock->foo(21, 42));
     }
 
+    public function testArrayIntegration()
+    {
+        $mock = $this->getMockBuilder(stdClass::class)
+                     ->setMethods(['foo'])
+                     ->getMock();
+
+        $mock->expects($this->any())
+             ->method('foo')
+             ->withConsecutiveArray([
+                 ['bar'],
+                 [21, 42],
+             ]);
+
+        $this->assertNull($mock->foo('bar'));
+        $this->assertNull($mock->foo(21, 42));
+    }
+
     public function testIntegrationWithLessAssertionsThanMethodCalls()
     {
         $mock = $this->getMockBuilder(stdClass::class)
